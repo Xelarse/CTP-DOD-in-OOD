e1a6a7cabd256aa2fc55df683c647d443dafe808
Alex Allman <xalexxd@hotmail.co.uk>
2020-04-10 20:17:06 +0100

    Poolable thread is header only

--- CreativeTechnologyProject/CreativeTechnologyProject/includes/PoolableThread.h
+++ CreativeTechnologyProject/CreativeTechnologyProject/includes/PoolableThread.h
@@ -7,21 +7,78 @@
 
 class PoolableThread
 {
-
 public:
+
+	////---------- Constructors and Destructor ----------////
 	PoolableThread() = delete;
-	PoolableThread(std::function<void()> orderedJobCallback);
-	~PoolableThread();
-    PoolableThread(PoolableThread const& other) = delete;
-    PoolableThread& operator=(PoolableThread const& other) = delete;
+	PoolableThread(const PoolableThread& otherCopy) = delete;
+	PoolableThread(const PoolableThread&& otherMove) = delete;
+	PoolableThread& operator= (const PoolableThread& otherCopy) = delete;
+	PoolableThread& operator= (const PoolableThread&& otherMove) = delete;
 
+	PoolableThread(std::function<void()> orderedJobCallback) : _orderedJobComplete(orderedJobCallback)
+	{
+		std::promise<void> exitPromise;
+		_exitFuture = exitPromise.get_future();
+		_thread = std::thread(&PoolableThread::ThreadLoop, this, std::move(exitPromise));
+		_thread.detach();
+	}
 
-	bool IsThreadIdle();
-	void RunTaskOnThread(std::function<void()> task, bool orderedTask = false);
-	void WaitForThreadToExit();
+	~PoolableThread()
+	{
+		WaitForThreadToExit();
+	}
 
+	////---------- Thread Interface ----------////
+	/*
+		Returns True if the thread is idle and ready for more work
+	*/
+	bool IsThreadIdle() { return _threadIdle; }
+
+	/*
+		Assigns the given function ptr to _task
+		The function the thread runs on loop checks this ptr and if its not null then runs it
+	*/
+	void RunTaskOnThread(std::function<void()> task, bool orderedTask = false)
+	{
+		_threadIdle = false;
+		_processingOrderedJob = orderedTask;
+		_task = task;
+	}
+		
 private:
-	void ThreadLoop(std::promise<void> exitPromise);
+
+	////---------- Thread Backend ----------////
+	/*
+		This is the function that is run by a thread.
+		While loop to ensure thread stays active until this class says otherwise
+	*/
+	void ThreadLoop(std::promise<void> exitPromise)
+	{
+		while (_threadAlive)
+		{
+			if (_task != nullptr)
+			{
+				_task();
+				_task = nullptr;
+				_threadIdle = true;
+				if (_processingOrderedJob) { _orderedJobComplete(); }
+			}
+			else { std::this_thread::sleep_for(std::chrono::milliseconds(1)); }
+		}
+		exitPromise.set_value();
+	}
+
+	/*
+		Used as a Safety net to ensure the detatched thread is terminated before,
+		destruction of this class instance.
+		Utilises the exit future passed into the thread on construction
+	*/
+	void WaitForThreadToExit()
+	{
+		_threadAlive = false;
+		_exitFuture.wait();
+	}
 
 	std::thread _thread;
 	std::atomic<bool> _threadIdle = true;
