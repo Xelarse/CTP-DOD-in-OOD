6cc81c257179b23d8a9873c79671a23f1a860c09
Alex Allman <xalexxd@hotmail.co.uk>
2020-04-21 16:29:13 +0100

    Added writeups to the project directory

--- CreativeTechnologyProject/CreativeTechnologyProject/source/BaseTest.cpp
+++ CreativeTechnologyProject/CreativeTechnologyProject/source/BaseTest.cpp
@@ -33,7 +33,7 @@ void BaseTest::RenderImguiWindow()
 		SanityCheckRunCount();
 		std::string desc = "Most recent update: " + std::to_string(results.back()) + "ms";
 		ImGui::Text(desc.c_str());
-		ImGui::PlotLines("Time taken in milliseconds.\nAxis run from 0 to 100 ms", results.data(), results.size(), 0, 0, 0.0f, 100.0f, ImVec2(0, 500));
+		ImGui::PlotLines("Time taken in milliseconds.\nAxis run from 0 to 100 ms", results.data(), results.size(), 0, 0, 0.0f, 200.0f, ImVec2(0, 500));
 	}
 }
 
